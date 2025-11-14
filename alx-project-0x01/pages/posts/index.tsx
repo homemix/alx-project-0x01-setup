eac67ed056064383b6ca0e9a1740dc2907860094
homemix <kenmutati@gmail.com>
2025-11-14 12:05:46 +0300

    ADD: Set up navigation between pages.

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "@/components/layout/Header";
 
 const IndexPage : React.FC = ()=>{
     return(
         <div className="flex justify-center items-center h-screen">
+            <Header/>
             <h1 className="text-7xl font-thin">Welcome IndexPage Page</h1>
         </div>
     )
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "@/components/layout/Header";
 
 const IndexUsers : React.FC = ()=>{
     return(
         <div className="flex justify-center items-center h-screen">
+            <Header/>
             <h1 className="text-7xl font-thin">Welcome Indexusers Page</h1>
         </div>
     )
